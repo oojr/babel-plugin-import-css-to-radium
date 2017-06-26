435c37d238b9fdef82738a744928245876f3661b
Olajide Ogundipe Jr <o.ogundipejr@gmail.com>
2017-06-26 00:42:23 -0400

    convert css imports to radium

--- index.js
+++ index.js
@@ -1,117 +1,140 @@
-var fs = require('fs')
-var less = require('less')
-var path = require('path')
-var sass = require('node-sass')
-var deasync = require('deasync')
-var cssToJss = require('jss-cli/lib/cssToJss')
-var requireResolve = require('require-resolve')
+var fs = require('fs');
+var less = require('less');
+var path = require('path');
+var sass = require('node-sass');
+var deasync = require('deasync');
+var cssToRadium = require('css-to-radium');
+var cssToJss = require('jss-cli/lib/cssToJss');
+var requireResolve = require('require-resolve');
 
 function toTree(t, obj) {
-    var props = []
+  var props = [];
 
-    for (var key in obj) {
-        var val = obj[key]
+  for (var key in obj) {
+    var val = obj[key];
 
-        if (val === null) {
-            val = t.nullLiteral()
-        } else {
-            var type = typeof(val)
-            
-            if (type ===  'undefined') {
-                continue
-            }
+    if (val === null) {
+      val = t.nullLiteral();
+    } else {
+      var type = typeof val;
 
-            switch(type) {
-                case 'string':
-                    val = t.stringLiteral(val)
-                    
-                    break
-                    
-                case 'number':
-                    val = t.numericLiteral(val)
-                    
-                    break
-                    
-                case 'boolean':
-                    val = t.booleanLiteral(val)
-                    
-                    break
-                    
-                default:
-                    val = toTree(t, val)
-            }
-        }
+      if (type === 'undefined') {
+        continue;
+      }
 
-        props.push(t.objectProperty(t.stringLiteral(key), val))
+      switch (type) {
+        case 'string':
+          val = t.stringLiteral(val);
+
+          break;
+
+        case 'number':
+          val = t.numericLiteral(val);
+
+          break;
+
+        case 'boolean':
+          val = t.booleanLiteral(val);
+
+          break;
+
+        default:
+          val = toTree(t, val);
+      }
     }
 
-    return t.objectExpression(props)
+    props.push(t.objectProperty(t.stringLiteral(key), val));
+  }
+
+  return t.objectExpression(props);
 }
 
 function lessToCss(file) {
-    var options = {
-        paths: [ path.dirname(file) ],
-        filename: path.basename(file)
-    }
+  var options = {
+    paths: [path.dirname(file)],
+    filename: path.basename(file)
+  };
 
-    return deasync(less.render.bind(less))(fs.readFileSync(file).toString(), options).css.toString()
+  return deasync(less.render.bind(less))(
+    fs.readFileSync(file).toString(),
+    options
+  ).css.toString();
 }
 
 function sassToCss(file) {
-    var options = {
-        file: file,
-        includePaths: [ path.dirname(file) ]
-    }
+  var options = {
+    file: file,
+    includePaths: [path.dirname(file)]
+  };
 
-    return deasync(sass.render.bind(sass))(options).css.toString()
+  return deasync(sass.render.bind(sass))(options).css.toString();
 }
 
-module.exports = function (babel) {
-    var t = babel.types
+module.exports = function(babel) {
+  var t = babel.types;
 
-    return {
-        visitor: {
-            ImportDeclaration: {
-                // pretty much guessing what input paramters are called
+  return {
+    visitor: {
+      ImportDeclaration: {
+        // pretty much guessing what input paramters are called
 
-                exit: function(decl, file) {
-                    var node = decl.node
+        exit: function(decl, file) {
+          var node = decl.node;
 
-                    if (node.source.value.endsWith('.css')) {
-                        // everything you see here is a complete guesswork but
-                        // that is what you get without proper documentation -
-                        // #babel6
+          if (node.source.value.endsWith('.css')) {
+            // everything you see here is a complete guesswork but
+            // that is what you get without proper documentation -
+            // #babel6
 
-                        var mod = requireResolve(node.source.value, path.resolve(file.file.opts.filename))
-                        var id = t.identifier(node.specifiers[0].local.name)
-                        var value = toTree(t, cssToJss({code: fs.readFileSync(mod.src).toString()})) // due to bugs we cannot use t.valueToNode
+            var mod = requireResolve(
+              node.source.value,
+              path.resolve(file.file.opts.filename)
+            );
+            var id = t.identifier(node.specifiers[0].local.name);
+            var value = toTree(
+              t,
+              cssToRadium(fs.readFileSync(mod.src).toString())
+            ); // due to bugs we cannot use t.valueToNode
 
-                        decl.replaceWith(t.variableDeclaration('var', [t.variableDeclarator(id, value)]))
-                    } else
-                    if (node.source.value.endsWith('.less')) {
-                        // everything you see here is a complete guesswork but
-                        // that is what you get without proper documentation -
-                        // #babel6
+            decl.replaceWith(
+              t.variableDeclaration('var', [t.variableDeclarator(id, value)])
+            );
+          } else if (node.source.value.endsWith('.less')) {
+            // everything you see here is a complete guesswork but
+            // that is what you get without proper documentation -
+            // #babel6
 
-                        var mod = requireResolve(node.source.value, path.resolve(file.file.opts.filename))
-                        var id = t.identifier(node.specifiers[0].local.name)
-                        var value = toTree(t, cssToJss({code: lessToCss(mod.src)})) // due to bugs we cannot use t.valueToNode
+            var mod = requireResolve(
+              node.source.value,
+              path.resolve(file.file.opts.filename)
+            );
+            var id = t.identifier(node.specifiers[0].local.name);
+            var value = toTree(t, cssToJss({ code: lessToCss(mod.src) })); // due to bugs we cannot use t.valueToNode
 
-                        decl.replaceWith(t.variableDeclaration('var', [t.variableDeclarator(id, value)]))
-                    } else
-                    if (node.source.value.endsWith('.sass') || node.source.value.endsWith('.scss')) {
-                        // everything you see here is a complete guesswork but
-                        // that is what you get without proper documentation -
-                        // #babel6
+            decl.replaceWith(
+              t.variableDeclaration('var', [t.variableDeclarator(id, value)])
+            );
+          } else if (
+            node.source.value.endsWith('.sass') ||
+            node.source.value.endsWith('.scss')
+          ) {
+            // everything you see here is a complete guesswork but
+            // that is what you get without proper documentation -
+            // #babel6
 
-                        var mod = requireResolve(node.source.value, path.resolve(file.file.opts.filename))
-                        var id = t.identifier(node.specifiers[0].local.name)
-                        var value = toTree(t, cssToJss({code: sassToCss(mod.src)})) // due to bugs we cannot use t.valueToNode
+            var mod = requireResolve(
+              node.source.value,
+              path.resolve(file.file.opts.filename)
+            );
+            var id = t.identifier(node.specifiers[0].local.name);
+            var value = toTree(t, cssToJss({ code: sassToCss(mod.src) })); // due to bugs we cannot use t.valueToNode
 
-                        decl.replaceWith(t.variableDeclaration('var', [t.variableDeclarator(id, value)]))
-                    }
-                }
-            }
+            decl.replaceWith(
+              t.variableDeclaration('var', [t.variableDeclarator(id, value)])
+            );
+          }
         }
+      }
     }
-}
+  };
+};
